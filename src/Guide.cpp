cdff89163136cb6c1ae882cd94ffba1a37f2bf46
RyanEnti <ryan.palazon@enti.cat>
2021-10-19 13:19:49 +0200

    update

---
+++ src/Guide.cpp
@@ -0,0 +1,14 @@
+//WeightedBlending::calculateSteeringForce(...) {
+//	for steering in steering_behabiors{
+//		sum += steering.calculateSteeringForce()
+//	}
+//		return sum
+//}
+//
+//WeightedBlending::calculateSteeringForce(...) {
+//	for steering in steering_behabiors{
+//		f = steering.calculateSteeringForce()
+//		if f > THRESHOLD
+//			return f
+//	}
+//}
